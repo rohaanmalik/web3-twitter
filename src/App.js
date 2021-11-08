21b4a0dee3efc83abc5a7a1816e773e7f12686de
rohaan malik <rohaanmalik7@gmail.com>
2021-11-08 08:37:49 -0500

    building the base

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
 // Constants
-const TWITTER_HANDLE = '_buildspace';
+const TWITTER_HANDLE = 'rohaan7malik';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
 const App = () => {
@@ -10,9 +10,9 @@ const App = () => {
     <div className="App">
       <div className="container">
         <div className="header-container">
-          <p className="header">🖼 GIF Portal</p>
+          <p className="header">Welcome to Web3 Twitter</p>
           <p className="sub-text">
-            View your GIF collection in the metaverse ✨
+            A place where freedom of speech truely exists
           </p>
         </div>
         <div className="footer-container">
@@ -22,7 +22,7 @@ const App = () => {
             href={TWITTER_LINK}
             target="_blank"
             rel="noreferrer"
-          >{`built on @${TWITTER_HANDLE}`}</a>
+          >{`built by @${TWITTER_HANDLE}`}</a>
         </div>
       </div>
     </div>
